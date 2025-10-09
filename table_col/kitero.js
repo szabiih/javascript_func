3c1753ba17dbdd2983c88eed68f08bc636c4026e
Székely Szabolcs <szekely.szabolcs_2022b@bolyai-szakkozep.hu>
2025-10-09 23:00:15 +0200

    hazi feladat

--- table_col/kitero.js
+++ table_col/kitero.js
@@ -35,3 +35,101 @@ const a = {
 console.log(a.name);
 console.log(a.age);
 console.log(a);
+
+//  H F ->
+/**
+ * @type {{brand:string,model:string,year:string,designer1:string,designer2?:string}[]}
+ */
+const arr = [
+    {
+        brand: 'Rolex',
+        model: 'Daytona',
+        year: '1963',
+        designer1: 'Rolex Design Team'
+    },
+    {
+        brand: 'Omega',
+        model: 'Speedmaster',
+        year: '1957',
+        designer1: 'Claude Baillod',
+        designer2: 'Pierre Moinat'
+    },
+    {
+        brand: 'Seiko',
+        model: 'Astron',
+        year: '1969',
+        designer1: 'Suwa Seikosha Team',
+    },
+    {
+        brand: 'Tissot',
+        model: 'Seastar',
+        year: '1960',
+        designer1: 'Charles Tissot',
+        designer2: 'Jacques-David LeCoultre'
+    }
+]
+
+//  ...
+const table = document.createElement('table');
+document.body.appendChild(table);
+
+const thead = document.createElement('thead');
+table.appendChild(thead);
+
+//  Row
+const tr01 = document.createElement('tr');
+thead.appendChild(tr01);
+
+//  Header cells
+const th01 = document.createElement('th');
+th01.innerText = 'Márka';
+tr01.appendChild(th01);
+
+const th02 = document.createElement('th');
+th02.innerText = 'Modell';
+tr01.appendChild(th02);
+
+const th03 = document.createElement('th');
+th03.innerText = 'Megjelenés éve';
+tr01.appendChild(th03);
+
+const th04 = document.createElement('th');
+th04.innerText = 'Tervező(k)';
+th04.colSpan = 2;
+tr01.appendChild(th04);
+
+//  ...
+const tbody = document.createElement('tbody');
+table.appendChild(tbody);
+
+//  For Loop
+for (const object of arr) {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+
+    const td01 = document.createElement('td');
+    td01.innerText = object.brand;
+    tr.appendChild(td01);
+
+    const td02 = document.createElement('td');
+    td02.innerText = object.model;
+    tr.appendChild(td02);
+
+    const td03 = document.createElement('td');
+    td03.innerText = object.year;
+    tr.appendChild(td03);
+
+    const td04 = document.createElement('td');
+    td04.innerText = object.designer1;
+    
+    if (object.designer2 === undefined) {
+        td04.colSpan = 2;
+        tr.appendChild(td04);
+    }
+    else {
+        tr.appendChild(td04);
+        const td05 = document.createElement('td');
+        td05.innerText = object.designer2;
+        tr.appendChild(td05);
+    }
+}
