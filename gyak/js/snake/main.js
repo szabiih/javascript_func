cdf35acb13ced877ab0000a28c4b01800ed9cf17
Székely Szabolcs <szekely.szabolcs_2022b@bolyai-szakkozep.hu>
2025-11-23 20:00:04 +0100

    mozgás befejezése

--- gyak/js/snake/main.js
+++ gyak/js/snake/main.js
@@ -49,31 +49,37 @@ function update(){
     context.fillStyle = 'black';
     context.fillRect(0, 0, board.width, board.height);
 
+    //  the food is continuously updated
+    context.fillStyle = 'red';
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    //  logic for checking collision between two squares
+    if (snakeX == foodX && snakeY == foodY){
+        placeFood();
+    }
+
     //  the snake head is continuously updated
     context.fillStyle = 'lime';
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
-    //  the food is continuously updated
-    context.fillStyle = 'red';
-    context.fillRect(foodX, foodY, blockSize, blockSize);
 }
 
 function changeDirection(e){
-    if (e.code == 'ArrowUp'){
+    if (e.code == 'ArrowUp' && velocityY != 1){
         velocityX = 0;
         velocityY = -1;
     }
-    else if (e.code == 'ArrowDown'){
+    else if (e.code == 'ArrowDown' && velocityY != -1){
         velocityX = 0;
         velocityY = 1;
     }
-    else if (e.code == 'ArrowLeft'){
+    else if (e.code == 'ArrowLeft' && velocityX != 1){
         velocityX = -1;
         velocityY = 0;
     }
-    else if (e.code == 'ArrowRight'){
+    else if (e.code == 'ArrowRight' && velocityX != -1){
         velocityX = 1;
         velocityY = 0;
     }
